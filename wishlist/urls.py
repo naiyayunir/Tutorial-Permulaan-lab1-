3b58a20169cc41050d97b071b2c5b9777cb0ca49
naiyayunir <ayunirnaiya@gmail.com>
2022-09-22 15:51:42 +0700

    tutorial 3 finish

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from wishlist.views import show_wishlist
+from wishlist.views import register
+from wishlist.views import login_user
+from wishlist.views import logout_user
 
 app_name = 'wishlist'
 
 urlpatterns = [
     path('', show_wishlist, name='show_wishlist'),
+    path('register/', register, name='register'),
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
 ]
