9e37fac2894dc948b835b1f9b0459607a0dd0f70
naiyayunir <ayunirnaiya@gmail.com>
2022-09-29 16:26:50 +0700

    finish lab 4

--- wishlist/views.py
+++ wishlist/views.py
@@ -18,7 +18,7 @@ def show_wishlist(request):
     data_barang_wishlist = BarangWishlist.objects.all()
     context = {
     'list_barang': data_barang_wishlist,
-    'nama': 'Naiya Dwita Ayunir',
+    'nama':'Naiya Dwita Ayunir',
     'last_login': request.COOKIES['last_login'],
     }
     return render(request, "wishlist.html",context)
